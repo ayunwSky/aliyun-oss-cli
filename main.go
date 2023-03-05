8f20ab1286b8973645a235d5e1ada944fde31a53
ayunwSky <ayunwsky@qq.com>
2023-03-05 03:38:11 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"upload_oss/config"
+)
+
+func main() {
+	config, err := config.LoadConfig("./config/config.yaml")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	fmt.Println(config.Oss.Endpoint)
+	fmt.Println(config.Oss.AccessKeyID)
+	fmt.Println(config.Oss.AccessKeySecret)
+	fmt.Println(config.Oss.BucketName)
+}
