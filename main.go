f6aa67b0c3fa1244e87a092f2b4342d6bf5f6707
ayunwSky <ayunwsky@qq.com>
2023-03-05 03:41:31 -0300

    fix: 增加输出所有配置文件内容

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 		log.Fatalf("failed to load config: %v", err)
 	}
 
+	fmt.Println(config.Oss)
 	fmt.Println(config.Oss.Endpoint)
 	fmt.Println(config.Oss.AccessKeyID)
 	fmt.Println(config.Oss.AccessKeySecret)
