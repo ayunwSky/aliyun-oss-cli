5bbeb8c281df853ee49492ae7f3146e724d08cbf
ayunwSky <ayunwsky@qq.com>
2023-03-05 03:43:03 -0300

    fix: 增加空行

--- config/config.go
+++ config/config.go
@@ -21,6 +21,7 @@ func LoadConfig(configPath string) (*Config, error) {
 	// 使用 viper 解析 配置文件
 	viper.SetConfigType("yaml")
 	viper.SetConfigFile(configPath)
+	
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, fmt.Errorf("failed to read config file: %v", err)
 	}
